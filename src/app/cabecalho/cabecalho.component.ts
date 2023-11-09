89b9d0fd60e86280e82b2ac3370b3afc6cad0538
MarshiMarshi <78913373+MarshiMarshi@users.noreply.github.com>
2023-11-09 16:28:41 -0300

    Mudando logo em diferentes tamnhos de tela

--- src/app/cabecalho/cabecalho.component.ts
+++ src/app/cabecalho/cabecalho.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { ScrollServiceService } from '../scroll-service.service';
 import { HomeComponent } from '../home/home.component';
 
@@ -8,6 +8,22 @@ import { HomeComponent } from '../home/home.component';
   styleUrls: ['./cabecalho.component.css'],
 })
 export class CabecalhoComponent {
+  mobile = false
 
+  ngOnInit() {
+    if (window.screen.width < 992) {
+      this.mobile = true
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    if (window.innerWidth < 992) {
+      this.mobile = true
+    }
+    else {
+      this.mobile = false
+    }
+  }
 }
 
