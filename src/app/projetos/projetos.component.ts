c391ae8f304b853868d900ad9bc2ebc03a4bcf3a
MarshiMarshi <78913373+MarshiMarshi@users.noreply.github.com>
2023-11-10 02:28:11 -0300

    Mexendo no carrossel

--- src/app/projetos/projetos.component.ts
+++ src/app/projetos/projetos.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, ViewChild } from '@angular/core';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-projetos',
@@ -6,11 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./projetos.component.css']
 })
 export class ProjetosComponent {
-  slides = [
-    { image: 'path/to/image1.jpg' },
-    { image: 'path/to/image2.jpg' },
-    { image: 'path/to/image3.jpg' },
-    { image: 'path/to/image4.jpg' },
-    // Add more slides as needed
+  @ViewChild('carrosselProjetos') carrosselProjetos!: ElementRef;
+
+  carousel: any; // Adjust the type based on the Bootstrap version you are using
+  carouselWidth: number = 0;
+  carouselItemWidth: number = 0;
+  scrollPosition: number = 0;
+  teste = false
+
+  projetos = [
+    { image: 'assets/Logo-Mind.png', name: 'Mind' },
+    { image: 'assets/Logo-Mind.png', name: 'Mind 2' },
+    { image: 'assets/Logo-Mind.png', name: 'Mind 3' },
+    { image: 'assets/Logo-Mind.png', name: 'Mind 4' },
+    { image: 'assets/Logo-Mind.png', name: 'Mind 5' },
+    { image: 'assets/Logo-Mind.png', name: 'Mind 6' },
   ];
+  itemsPerSlide = 5;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(event: any) {
+    const element = this.carrosselProjetos.nativeElement
+    const idx = event.relatedTarget.index
+    const totalItems = element.querySelectorAll('.carousel-item').length
+    
+    if (idx >= totalItems - (this.itemsPerSlide - 1)) {
+      const it = this.itemsPerSlide - (totalItems - idx)
+      for (let i = 0; i < it; i++) {
+        if (event.direction === 'left') {
+          this.renderer.appendChild(element.querySelector('.carousel-inner'), element.querySelector('.carousel-item'));
+        } else {
+          this.renderer.appendChild(element.querySelector('.carousel-inner'), element.querySelector('.carousel-item:first-child'));
+        }
+      }
+    }
+  }
+
+  /*private animateScroll(): void {
+    if (this.carrosselProjetos) {
+      const innerElement = this.carrosselProjetos.nativeElement.querySelector('.carousel-inner');
+      this.renderer.setStyle(innerElement, 'scroll-behavior', 'smooth');
+      innerElement.scrollLeft = this.scrollPosition;
+
+      // Reset scroll-behavior to auto after animation
+      setTimeout(() => {
+        this.renderer.removeStyle(innerElement, 'scroll-behavior');
+      }, 600);
+    }
+  }*/
 }
