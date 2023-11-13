43b545d9b96a9fdd88141b271a3b85156a801138
Gustavo G Rodrigues <gustavogr180@gmail.com>
2023-11-13 11:47:21 -0300

    Deixando projetos responsivo

--- src/app/projetos/projetos.component.ts
+++ src/app/projetos/projetos.component.ts
@@ -1,31 +1,43 @@
-import { Component, OnInit, Renderer2, ElementRef, ViewChild, HostListener } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, ViewChild, HostListener, AfterViewInit, Input, ChangeDetectorRef } from '@angular/core';
 import * as bootstrap from 'bootstrap';
+import { CarouselComponent, CarouselModule } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-projetos',
   templateUrl: './projetos.component.html',
   styleUrls: ['./projetos.component.css']
 })
-export class ProjetosComponent {
+export class ProjetosComponent implements OnInit {
+  @ViewChild('carrosselProjetos') carrossel!: CarouselComponent
 
-  itemsPerSlide = 3
+  private _itemsPerSlide!: number;
+  mobile = true
+  quan = 0
+
+  @Input()
+  set itemsPerSlide(value: number) {
+    this._itemsPerSlide = value
+  }
+
+  get itemsPerSlide(): number {
+    return this._itemsPerSlide
+  }
 
   projetos = [
-    { image: 'assets/Logo-Mind.png', name: 'Mind' },
-    { image: 'assets/Logo-EuDiversidade.png', name: 'Mind 2' },
-    { image: 'assets/Logo-Mind.png', name: 'Mind 3' },
-    { image: 'assets/Logo-Mind.png', name: 'Mind 4' },
-    { image: 'assets/Logo-Mind.png', name: 'Mind 5' },
-    { image: 'assets/Logo-Mind.png', name: 'Mind 6' },
+    { id: 0, image: 'assets/Logo-Mind.png', name: 'Mind' },
+    { id: 1, image: 'assets/Logo-EuDiversidade.png', name: 'Mind 2' },
+    { id: 2, image: 'assets/Logo-Mind.png', name: 'Mind 3' },
+    { id: 3, image: 'assets/Logo-Mind.png', name: 'Mind 4' },
+    { id: 4, image: 'assets/Logo-Mind.png', name: 'Mind 5' },
+    { id: 5, image: 'assets/Logo-Mind.png', name: 'Mind 6' },
   ];
 
-  ngOnInit(event: any) {
-    if (window.innerWidth < 650)
-      this.itemsPerSlide = 1
+  ngOnInit() {
+    this.mobile = window.innerWidth < 650 ? true : false
   }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) : void {
-    this.itemsPerSlide = window.innerWidth >= 650 ? 3 : 1
+    this.mobile = window.innerWidth < 650 ? true : false
   }
 }
