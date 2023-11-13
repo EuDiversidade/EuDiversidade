746c053dbde885c8d8867889ba1dfa4938f5f501
Gustavo G Rodrigues <gustavogr1806@gmail.com>
2023-11-13 09:06:42 -0300

    Mexendo na tela projetos

--- src/app/projetos/projetos.component.ts
+++ src/app/projetos/projetos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, ViewChild, HostListener } from '@angular/core';
 import * as bootstrap from 'bootstrap';
 
 @Component({
@@ -7,13 +7,8 @@ import * as bootstrap from 'bootstrap';
   styleUrls: ['./projetos.component.css']
 })
 export class ProjetosComponent {
-  @ViewChild('carrosselProjetos') carrossel!: ElementRef;
 
-  carousel: any; // Adjust the type based on the Bootstrap version you are using
-  carouselWidth: number = 0;
-  carouselItemWidth: number = 0;
-  scrollPosition: number = 0;
-  teste = false
+  itemsPerSlide = 3
 
   projetos = [
     { image: 'assets/Logo-Mind.png', name: 'Mind' },
@@ -23,37 +18,14 @@ export class ProjetosComponent {
     { image: 'assets/Logo-Mind.png', name: 'Mind 5' },
     { image: 'assets/Logo-Mind.png', name: 'Mind 6' },
   ];
-  itemsPerSlide = 3;
-
-  constructor(private renderer: Renderer2) {}
 
   ngOnInit(event: any) {
-    const element = this.carrossel.nativeElement
-    const idx = event.relatedTarget.index
-    const totalItems = element.querySelectorAll('.carousel-item').length
-    
-    if (idx >= totalItems - (this.itemsPerSlide - 1)) {
-      const it = this.itemsPerSlide - (totalItems - idx)
-      for (let i = 0; i < it; i++) {
-        if (event.direction === 'left') {
-          this.renderer.appendChild(element.querySelector('.carousel-inner'), element.querySelector('.carousel-item'));
-        } else {
-          this.renderer.appendChild(element.querySelector('.carousel-inner'), element.querySelector('.carousel-item:first-child'));
-        }
-      }
-    }
+    if (window.innerWidth < 650)
+      this.itemsPerSlide = 1
   }
 
-  /*private animateScroll(): void {
-    if (this.carrossel) {
-      const innerElement = this.carrossel.nativeElement.querySelector('.carousel-inner');
-      this.renderer.setStyle(innerElement, 'scroll-behavior', 'smooth');
-      innerElement.scrollLeft = this.scrollPosition;
-
-      // Reset scroll-behavior to auto after animation
-      setTimeout(() => {
-        this.renderer.removeStyle(innerElement, 'scroll-behavior');
-      }, 600);
-    }
-  }*/
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) : void {
+    this.itemsPerSlide = window.innerWidth >= 650 ? 3 : 1
+  }
 }
